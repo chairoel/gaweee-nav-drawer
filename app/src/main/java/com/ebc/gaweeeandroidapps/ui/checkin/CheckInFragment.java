55fc9baa4828d4d881d2f0a56a64516e6e087472
chairoel <chairoelamri2610@gmail.com>
2020-12-21 13:01:59 +0700

    -check in fragment awal

--- app/src/main/java/com/ebc/gaweeeandroidapps/ui/checkin/CheckInFragment.java
+++ app/src/main/java/com/ebc/gaweeeandroidapps/ui/checkin/CheckInFragment.java
@@ -22,14 +22,14 @@ public class CheckInFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         checkInViewModel =
                 new ViewModelProvider(this).get(CheckInViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_check_in, container, false);
-        final TextView textView = root.findViewById(R.id.text_check_in);
+        View view = inflater.inflate(R.layout.fragment_check_in, container, false);
+        /*final TextView textView = root.findViewById(R.id.text_check_in);
         checkInViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
-        return root;
+        });*/
+        return view;
     }
 }
